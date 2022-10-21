b76976e684a84550fc6a9060b915ca6faf96db7c
Eduardo Hass <eduardo.hass@outlook.com>
2022-10-21 11:21:14 +0100

    feat: removing backend workflow

--- terraform_backend/dinamodb-table.tf
+++ terraform_backend/dinamodb-table.tf
@@ -1,12 +1,12 @@
 resource "aws_dynamodb_table" "dynamodb-backend" {
-  name = "dynamodb-backend"
-  hash_key = "LockID"
-  read_capacity = 20
+  name           = "dynamodb-backend"
+  hash_key       = "LockID"
+  read_capacity  = 20
   write_capacity = 20
   attribute {
     name = "LockID"
     type = "S"
-  } 
+  }
   tags = {
     Name = "dynamodb-backend"
   }
--- terraform_backend/main.tf
+++ terraform_backend/main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-    profile    = "default"
-    region     = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "bucket-backend" {
-    bucket = "comphass-infra-terraform-state-backend"
-    versioning {
-        enabled = true
-    }
-    lifecycle {
-        prevent_destroy = false // need to change to true when dev finish
-    }
-    tags = {
-        Name = "backend-S3"
-    }
+  bucket = "comphass-infra-terraform-state-backend"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = false // need to change to true when dev finish
+  }
+  tags = {
+    Name = "backend-S3"
+  }
 }
